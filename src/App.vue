1a695771dd26ca00f6fcbfe566d288f511d2c11a
huluobu77 <2698640037@qq.com>
2024-10-09 16:29:29 +0800

    添加了test

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Hello from './components/Hello.vue';
+import Test  from './components/Test.vue';
 </script>
 
 <template>
   <h2>音乐网站</h2>
   <Hello />
+  <Test />
 </template>
 
 <style scoped></style>
