e2ead436677a54d4688678683169064ad593c961
xqTina <3508729644@qq.com>
2024-10-13 11:28:00 +0800

    elment-plus添加了自动导入

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import './style.css'
 import App from './App.vue'
 
@@ -6,8 +8,9 @@ import router from '@/router.js'
 
 const app = createApp(App)
 app.use(router)
+app.use(ElementPlus)
 
-app.config.errorHandler = (err)=>{
+app.config.errorHandler = (err) => {
     console.log('捕获到错误了')
 }
 app.mount('#app')
