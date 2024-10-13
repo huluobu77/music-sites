abc0e1259b18b0e2b6e2aeed263bc36cc33c24cc
xqTina <3508729644@qq.com>
2024-10-13 13:25:51 +0800

    save

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,13 +1,5 @@
 <template>
     <div>首页</div>
-    <div class="mb-4">
-        <el-button round>Round</el-button>
-        <el-button type="primary" round>Primary</el-button>
-        <el-button type="success" round>Success</el-button>
-        <el-button type="info" round>Info</el-button>
-        <el-button type="warning" round>Warning</el-button>
-        <el-button type="danger" round>Danger</el-button>
-    </div>
 </template>
 
 <script setup>
